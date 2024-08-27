b5118e55dcbe0cac343f48fce9851113fed16a0b
David Maldonado <gorossy07@gmail.com>
2024-08-27 15:00:51 -0500

    fix: prod

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
+import os
 from flask import Flask, request, jsonify
 import yt_dlp
-import os
-
 
 app = Flask(__name__)
 
 @app.route('/extract', methods=['POST'])
 def extract_video_data():
     url = request.json.get('url')
-    
     if not url:
         return jsonify({'error': 'No URL provided'}), 400
 
