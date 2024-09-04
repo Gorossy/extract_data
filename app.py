a0737c7b3e8ab91db645f82d79b21f4af821f8b6
David Maldonado <gorossy07@gmail.com>
2024-09-03 20:27:25 -0500

    fix: upload_date

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 import yt_dlp
 import instaloader
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -38,7 +39,7 @@ def resolve_tiktok_url(url):
         response = requests.get(url, allow_redirects=True)
         return response.url
     except requests.RequestException as e:
-        return url  # Devuelve la URL original si falla la resolución
+        return url
 
 def extract_using_ytdlp(url):
     scraperapi_key = "1c8a4d9d153ef5b9950f3f2324ebac45"
@@ -53,13 +54,18 @@ def extract_using_ytdlp(url):
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
+        
+        upload_date = info.get('upload_date')
+        if upload_date:
+            upload_date = datetime.strptime(upload_date, '%Y%m%d').strftime('%Y-%m-%d')
+        
         return {
             'url': url,
             'title': info.get('title'),
             'duration': info.get('duration'),
             'view_count': info.get('view_count'),
             'like_count': info.get('like_count'),
-            'upload_date': info.get('upload_date'),
+            'upload_date': upload_date,
             'author': info.get('uploader'),
             'comments': info.get('comment_count'),
             'shares': info.get('repost_count')
@@ -71,7 +77,6 @@ def extract_using_instaloader(url):
     scraperapi_key = "1c8a4d9d153ef5b9950f3f2324ebac45"
     proxy_url = f"http://scraperapi:{scraperapi_key}@proxy-server.scraperapi.com:8001"
 
-    # Crear una sesión con el proxy en Instaloader
     L = instaloader.Instaloader()
     L.context.proxy = proxy_url
     
